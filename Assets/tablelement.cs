d805730ef8f31019b53afa3a500ef4ddd47858d9
Ridek240 <ridek240zwami@gmail.com>
2025-03-31 12:02:53 +0200

    icons

--- Assets/tablelement.cs
+++ Assets/tablelement.cs
@@ -32,5 +32,14 @@ public class tablelement : MonoBehaviour
     {
         PlayerName.text = element.PlayerName;
         Champion.text = element.Champion;
+        KDA.text = $"{element.kills}/{element.deaths}/{element.assists}";
+        KDR.text = $"{element.kills + element.assists / element.deaths}";
+        KP.text = $"{element.KillPartipitation * 100:F2}%";
+        DMG.text = element.Damage.ToString();
+        VS.text = element.VisionScore.ToString();
+        VSM.text = $"{element.VisionScoreMinute:F2}";
+        Minion.text = element.Minions.ToString();
+        GPM.text = element.GoldPerMinute.ToString();
+        ChampionIcon.sprite = Resources.Load<Sprite>($"LoLSmall/{element.Champion}");
     }
 }
